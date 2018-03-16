511dcf3aa8bac44380d4e7b1cbe8ace699b506c6
Kyle Corsi <kyle.corsi@gmail.com>
2018-03-16 20:50:16 +0000

    Added Common assembly components

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+ 
+[assembly: AssemblyCompany("DataMotion")]
+[assembly: AssemblyProduct("DataMotion Web Library")]
+[assembly: AssemblyCopyright("DataMotion")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+ 
+[assembly: ComVisible(false)]
+ 
+[assembly: AssemblyVersion("1.2.3.4")] //Will be replaced
+ 
+[assembly: NeutralResourcesLanguage("en-US")]
