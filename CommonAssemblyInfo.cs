ff3b8059905059e90ec4e6086840e758341d44d2
Kyle Corsi <kyle.corsi@gmail.com>
2018-03-16 20:51:59 +0000

    Update CommonAssemblyInfo.cs

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -11,5 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
  
 [assembly: AssemblyVersion("1.2.3.4")] //Will be replaced
+[assembly: AssemblyFileVersion("1.0.0.0")] //Will be replaced
  
 [assembly: NeutralResourcesLanguage("en-US")]
